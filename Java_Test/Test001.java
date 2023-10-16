24c856563d03ae59c1a70d11a200c1ad8ba82a53
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-10-16 12:25:56 +0900

    Initial commit

---
+++ Java_Test/Test001.java
@@ -0,0 +1,62 @@
+package Java_Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    파스칼의 삼각형 
+*/
+public class Test001
+{
+    public static void main(String args[])
+    {
+        List<Integer> arr = getRow(33);
+
+        if(arr != null)
+        {
+            for(int k = 0; k < arr.size(); k ++)
+            {
+                System.out.println(arr.get(k));
+            }
+        }
+        else
+        {
+            System.out.println("에러");
+        }
+        
+    }
+
+     public static List<Integer> getRow(int rowIndex) {
+
+        List<Integer> result = null;
+
+        if(rowIndex <= 33 && rowIndex >= 0)
+        {
+            // 전체 반복 횟수
+            int arrWholeNum = rowIndex + 1;
+            
+            result = new ArrayList<Integer>();
+
+
+            for(int i = 1; i <= arrWholeNum; i++)
+            {
+                int varNum = 0;
+                if(i == 1 || i == arrWholeNum)
+                {
+                    varNum = 1;
+                }
+                else
+                {
+                    varNum = rowIndex;
+                }
+
+                result.add(varNum);
+
+            }
+            
+        }
+        
+
+        return result;
+    }
+}
