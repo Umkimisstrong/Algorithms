8b877f0c367d9573aa713c48aa90d7ac5b58cb1c
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-04 17:50:23 +0900

    Test009 - Palindrome Number 좌우 정렬이 맞는 수인지 확인하기
    좌우 정렬 분석

---
+++ Java_Test/Test009.java
@@ -0,0 +1,31 @@
+package Java_Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Test009 
+    : 알고리즘 - Palindrome Number 좌우 정렬이 맞는 수인지 확인하기
+
+      - 입력 : 
+      - 출력 : 42
+*/
+
+
+public class Test009
+{
+    
+   
+    // main 메소드
+    public static void main(String args[])
+    {
+        Test009 obj = new Test009();
+        
+        // todo 
+        
+    }
+
+     
+
+   
+}
