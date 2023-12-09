064289a84a9cc2783920de7ca0ad34e143445dbf
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-09 09:41:20 +0900

    Test013 - first commit
    Test013 - first commit

---
+++ Java_Test/Test013.java
@@ -0,0 +1,23 @@
+package Java_Test;
+
+/*
+    Test013
+    : 알고리즘 - 
+*/
+
+
+public class Test012
+{
+
+    // main 메소드
+    public static void main(String args[])
+    {
+        Test013 obj = new Test013();
+        
+       
+      
+        
+    }
+    
+   
+}
