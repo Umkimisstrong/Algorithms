392df841cd88f37c60095c0253dcd2ef5f7d3066
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-10 19:54:29 +0900

    Test013 - commit
    Test013 - commit

--- Java_Test/Test013.java
+++ Java_Test/Test013.java
@@ -2,12 +2,21 @@ package Java_Test;
 
 /*
     Test013
-    : 알고리즘 - 
+    : 알고리즘 - 로마형을 정수형으로 바꾸기(Roman To Integer)
 */
 
 
 public class Test013
 {
+
+    public int romanToInt(String s)
+    {
+        int result = 0;
+
+        
+        return result;
+
+    }
 
     // main 메소드
     public static void main(String args[])
