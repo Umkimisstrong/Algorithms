8c1a943b74040efe4a330d202979c25a5a9bd4b3
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-13 10:48:03 +0900

    Test015 - first commit
    Test015 - first commit

---
+++ Java_Test/Test015.java
@@ -0,0 +1,30 @@
+package Java_Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    Test015
+    : 알고리즘 - 
+
+      - 입력 : ["flower", "flow", "flight"]
+      - 출력 : "fl"
+
+      - 입력 : ["dog", "racecar", "car"]
+      - 출력 : 없음
+*/      
+
+
+public class Test015
+{
+    // main 메소드
+    public static void main(String args[])
+    {
+        Test015 obj = new Test015();
+
+        System.out.println("");
+        
+    }
+    
+   
+}
