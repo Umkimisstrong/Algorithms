e7b872cf26023e1560620d00c3cc504d9bddb822
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-13 10:53:41 +0900

    Test015 - second commit
    Test015 - second commit

--- Java_Test/Test015.java
+++ Java_Test/Test015.java
@@ -1,22 +1,33 @@
 package Java_Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 /*
     Test015
-    : 알고리즘 - 
+    : 알고리즘 - 3SUM(삼중항의 합이 0이 되는 조합을 각기 다르게 출력한다.)
 
-      - 입력 : ["flower", "flow", "flight"]
-      - 출력 : "fl"
+      - 입력 : [-1, 0, 1, 2, -1, -4]
+      - 출력 : [[-1, -1, 2], [-1, 0, 1]]
 
-      - 입력 : ["dog", "racecar", "car"]
-      - 출력 : 없음
+      - 입력 : [0, 1, 1]
+      - 출력 : []
 */      
 
 
 public class Test015
 {
+
+    public ArrayList<ArrayList<Integer>> threeSum(int [] nums)
+    {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+
+
+        
+        return result;
+    }
     // main 메소드
     public static void main(String args[])
     {
