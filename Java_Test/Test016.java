1a5abaf3ebf5a1fe4821c913c1656259de6cbd12
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-16 00:50:46 +0900

    Test016 - first commit
    Test016 - first commit

---
+++ Java_Test/Test016.java
@@ -0,0 +1,34 @@
+package Java_Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/*
+    Test016
+    : 알고리즘 - 
+
+      - 입력 : 
+      - 출력 : 
+
+      - 입력 : 
+      - 출력 : 
+*/      
+
+
+public class Test016
+{
+
+    // main 메소드
+    public static void main(String args[])
+    {
+        Test016 obj = new Test016();
+        System.out.println(strResult);
+        
+    }
+    
+   
+}
