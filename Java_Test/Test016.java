2712ccff1d3c7af5f57f0ff1804849719671e1b9
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-18 10:37:18 +0900

    Test016 - commit
    Test016 - commit

--- Java_Test/Test016.java
+++ Java_Test/Test016.java
@@ -9,13 +9,14 @@ import java.util.Map;
 
 /*
     Test016
-    : 알고리즘 - 
+    : 알고리즘 - 3SUM Closest(삼중항의 합이 0이 되는 조합을 각기 다르게 출력한다. 합이 Taget 에 가까운 3개의 정수의 합을 반환한다.)
 
-      - 입력 : 
-      - 출력 : 
+      - 입력 : 배열 : [-1, 2, 1, -4], 타겟 : 1
+      - 출력 : 2
+      - 설명 : (-1 + 2 + 1) = 2, 배열의 요소들 중 합이 1에 가장 가까운 3개의 정수는 -1, 2, 1 이며 이의 합은 2이다.
 
-      - 입력 : 
-      - 출력 : 
+      - 입력 : 배열 : [0, 0, 0], 타겟 : 1
+      - 출력 : 0 
 */      
 
 
@@ -26,9 +27,17 @@ public class Test016
     public static void main(String args[])
     {
         Test016 obj = new Test016();
-        System.out.println(strResult);
+        System.out.println();
         
     }
     
+    public int threeSumClosest(int [] nums, int target)
+    {
+        int result = 0;
+
+
+        
+        return result;
+    }
    
 }
