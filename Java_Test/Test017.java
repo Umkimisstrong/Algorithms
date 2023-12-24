f8bbd0c8bec3a3cb726dc837f919ae8ebb72dcd7
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2023-12-24 10:57:40 +0900

    Test017 - last commit
    Test017 - last commit

--- Java_Test/Test017.java
+++ Java_Test/Test017.java
@@ -2,10 +2,12 @@ package Java_Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 /*
     Test017
@@ -26,19 +28,71 @@ import java.util.Map;
 public class Test017
 {
 
+
+
     // main 메소드
     public static void main(String args[])
     {
         Test017 obj = new Test017();
+        List<String> test = obj.letterCombinations("23");
 
-        
+        StringBuilder sb = new StringBuilder();
+        for(String result : test)
+        {
+            sb.append(result);
+            sb.append(". ");
+        }
+
+        System.out.println(sb.toString());
     }
 
-    public String [] leeterCombinations (String digits)
-    {
-        String [] result = null;
+    public String [] TABLE = new String [] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        return result;
+    public List<String> letterCombinations (String digits)
+    {
+        if(digits.isEmpty())
+            return Collections.emptyList();
+
+
+        char [] digitChars = digits.toCharArray();
+        int [] numbers = new int [digitChars.length];
+
+        for(int i= 0; i<digitChars.length;i++)
+        {
+            numbers[i] = digitChars[i] - '0';
+        }
+
+        int length = numbers.length;
+
+        return letterCombinationUtil(numbers, length);
+    }
+
+    public List<String> letterCombinationUtil ( int [] numbers, int length)
+    {
+        List<String> list = new ArrayList<>();
+        Queue<String> queue = new LinkedList<>();
+
+
+        queue.add("");
+        String letterCombination, letters;
+
+        while(!queue.isEmpty())
+        {
+            letterCombination = queue.poll();
+            if(letterCombination.length() == length)
+            {
+                list.add(letterCombination);
+                continue;
+            }
+            letters = TABLE [numbers[letterCombination.length()]];
+            for(int i = 0; i<letters.length(); i++)
+            {
+                queue.add(letterCombination + letters.charAt(i));
+            }
+        }
+
+        return list;
+    
     }
     
    
